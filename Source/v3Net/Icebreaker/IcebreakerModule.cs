e97dea50a5cca5230ac300c9192a6a61d0d88419
Adrian Solis <freddiemcdog@hotmail.com>
2019-04-14 15:46:44 -0700

    Set up autofac integration for WebAPI2.

---
+++ Source/v3Net/Icebreaker/IcebreakerModule.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IcebreakerModule.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Icebreaker
+{
+    using Autofac;
+
+    /// <summary>
+    /// Autofac Module
+    /// </summary>
+    public class IcebreakerModule : Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
