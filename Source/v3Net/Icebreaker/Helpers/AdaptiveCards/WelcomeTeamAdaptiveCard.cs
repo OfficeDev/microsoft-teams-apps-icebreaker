ec5775429a146f3ba48888b46cc3aeb056c62b8b
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-04-24 16:27:05 -0700

    Setting the final height

--- Source/v3Net/Icebreaker/Helpers/AdaptiveCards/WelcomeTeamAdaptiveCard.cs
+++ Source/v3Net/Icebreaker/Helpers/AdaptiveCards/WelcomeTeamAdaptiveCard.cs
@@ -42,7 +42,7 @@ namespace Icebreaker.Helpers.AdaptiveCards
             var tourTitle = Resources.WelcomeTourTitle;
             var appId = CloudConfigurationManager.GetSetting("AppId");
 
-            var tourUrl = $"https://teams.microsoft.com/l/task/" + appId + $"?url={htmlUrl}&" + $"height=600px&width=600px&title={tourTitle}";
+            var tourUrl = $"https://teams.microsoft.com/l/task/" + appId + $"?url={htmlUrl}&" + $"height=533px&width=600px&title={tourTitle}";
 
             var variablesToValues = new Dictionary<string, string>()
             {
