9ae907c73fecd56ea1305e565213edc623657fe3
Pranav Krishnamurthy <v-prkris@microsoft.com>
2019-08-19 17:22:16 -0700

    Having the necessary fix for Issue #45 in this commit - Using a custom extension method called CaseInsensitiveContains will enable to look for the extension in the UserPrincipalName of a person and the email address in the chat with goes to the EmailAddress as opposed to the UPN

---
+++ Source/Icebreaker/Helpers/Extensions.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Extensions.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Icebreaker.Helpers
+{
+    using System;
+
+    /// <summary>
+    /// This class contains custom methods that are being used.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Method that will look at having a specific text string, and having this done with case insensitive.
+        /// </summary>
+        /// <param name="text">The actual text to parse.</param>
+        /// <param name="value">The string that we are looking for.</param>
+        /// <param name="stringComparison">The string comparator.</param>
+        /// <returns>A value saying whether or not a specific string exists.</returns>
+        public static bool CaseInsensitiveContains(
+            this string text,
+            string value,
+            StringComparison stringComparison = StringComparison.CurrentCultureIgnoreCase)
+        {
+            return text.IndexOf(value, stringComparison) >= 0;
+        }
+    }
+}
