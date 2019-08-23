ef7fa9dae2dd97cb5621327549fe47dbc82a0f55
Pranav Krishnamurthy <v-prkris@microsoft.com>
2019-08-22 19:24:17 -0700

    Having things nearly done

---
+++ Source/Icebreaker/Cards/PairUpNotificationCard.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="PairUpNotificationCard.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Icebreaker.Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using AdaptiveCards;
+    using Microsoft.Bot.Schema;
+
+    /// <summary>
+    /// This class is responsible for creating the pairup notification card.
+    /// </summary>
+    public class PairUpNotificationCard
+    {
+        /// <summary>
+        /// Method that will return the attachment for the pairup notifications.
+        /// </summary>
+        /// <returns></returns>
+        public static Attachment GetCard()
+        {
+
+        }
+    }
+}
