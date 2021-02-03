a513274185ed822130973f68f531c88d73318796
Alison Fang <alisonfang16@gmail.com>
2021-02-03 10:56:47 -0500

    debug

--- Source/Icebreaker/Bot/IcebreakerBot.cs
+++ Source/Icebreaker/Bot/IcebreakerBot.cs
@@ -148,9 +148,9 @@ namespace Icebreaker.Bot
 
                         await this.SaveAddedToTeamAsync(serviceUrl, teamId, turnContext, personThatAddedBot);
                         await this.WelcomeTeam(turnContext, personThatAddedBot, cancellationToken);
-
+/*
                         var watch = new System.Diagnostics.Stopwatch();
-                        watch.Start();
+                        watch.Start();*/
 
                         var users = await ((BotFrameworkAdapter)turnContext.Adapter).GetConversationMembersAsync(turnContext, cancellationToken);
                         foreach (var user in users)
@@ -158,8 +158,8 @@ namespace Icebreaker.Bot
                             await this.WelcomeUser(turnContext, user.Id, teamsChannelData.Tenant.Id, teamsChannelData.Team.Id, cancellationToken);
                         }
 
-                        watch.Stop();
-                        this.telemetryClient.TrackTrace($"Time to notify all users: {watch.ElapsedMilliseconds} ms");
+/*                        watch.Stop();
+                        this.telemetryClient.TrackTrace($"Time to notify all users: {watch.ElapsedMilliseconds} ms");*/
                     }
                     else
                     {
