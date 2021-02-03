36bb622dcde69b3cda0b65342409bf745dd7004a
tranf <tranf@mit.edu>
2021-02-03 10:45:53 -0500

    Changes to icebreaker cards, UserInfo, and resources to add user profile

--- Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
+++ Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
@@ -29,34 +29,37 @@ namespace Icebreaker.Helpers.AdaptiveCards
         /// Creates the pairup notification card.
         /// </summary>
         /// <param name="teamName">The team name.</param>
-        /// <param name="sender">The user who will be sending this card.</param>
         /// <param name="recipient">The user who will be receiving this card.</param>
+        /// <param name="sender">The user who will be sending this card.</param>
+        /// <param name="senderProfile">The profile of the sender.</param>
         /// <param name="botDisplayName">The bot display name.</param>
         /// <returns>Pairup notification card</returns>
-        public static Attachment GetCard(string teamName, TeamsChannelAccount sender, TeamsChannelAccount recipient, string botDisplayName)
+        public static Attachment GetCard(string teamName, TeamsChannelAccount recipient, TeamsChannelAccount sender, string senderProfile, string botDisplayName)
         {
             // Guest users may not have their given name specified in AAD, so fall back to the full name if needed
-            var senderGivenName = string.IsNullOrEmpty(sender.GivenName) ? sender.Name : sender.GivenName;
             var recipientGivenName = string.IsNullOrEmpty(recipient.GivenName) ? recipient.Name : recipient.GivenName;
+            var senderGivenName = string.IsNullOrEmpty(sender.GivenName) ? sender.Name : sender.GivenName;
 
             // To start a chat with a guest user, use their external email, not the UPN
-            var recipientUpn = !IsGuestUser(recipient) ? recipient.UserPrincipalName : recipient.Email;
+            var senderUpn = !IsGuestUser(sender) ? sender.UserPrincipalName : sender.Email;
 
-            var meetingTitle = string.Format(Resources.MeetupTitle, senderGivenName, recipientGivenName);
+            var meetingTitle = string.Format(Resources.MeetupTitle, recipientGivenName, senderGivenName);
             var meetingContent = string.Format(Resources.MeetupContent, botDisplayName);
-            var meetingLink = "https://teams.microsoft.com/l/meeting/new?subject=" + Uri.EscapeDataString(meetingTitle) + "&attendees=" + recipientUpn + "&content=" + Uri.EscapeDataString(meetingContent);
+            var meetingLink = "https://teams.microsoft.com/l/meeting/new?subject=" + Uri.EscapeDataString(meetingTitle) + "&attendees=" + senderUpn + "&content=" + Uri.EscapeDataString(meetingContent);
 
             var cardData = new
             {
                 matchUpCardTitleContent = Resources.MatchUpCardTitleContent,
-                matchUpCardMatchedText = string.Format(Resources.MatchUpCardMatchedText, recipient.Name),
-                matchUpCardContentPart1 = string.Format(Resources.MatchUpCardContentPart1, botDisplayName, teamName, recipient.Name),
+                matchUpCardMatchedText = string.Format(Resources.MatchUpCardMatchedText, sender.Name),
+                matchUpCardContentPart1 = string.IsNullOrEmpty(senderProfile) ?
+                    string.Format(Resources.MatchUpCardContentPart1, botDisplayName, teamName, sender.Name) :
+                    string.Format(Resources.MatchUpCardContentPart1b, botDisplayName, teamName, sender.Name, senderProfile),
                 matchUpCardContentPart2 = Resources.MatchUpCardContentPart2,
-                chatWithMatchButtonText = string.Format(Resources.ChatWithMatchButtonText, recipientGivenName),
+                chatWithMatchButtonText = string.Format(Resources.ChatWithMatchButtonText, senderGivenName),
                 chatWithMessageGreeting = Resources.ChatWithMessageGreeting,
                 pauseMatchesButtonText = Resources.PausePairingsButtonText,
                 proposeMeetupButtonText = Resources.ProposeMeetupButtonText,
-                personUpn = recipientUpn,
+                personUpn = senderUpn,
                 meetingLink,
             };
 
