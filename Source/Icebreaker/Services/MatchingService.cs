ddc33b9699d5e4484556962a8032a827cc9ce741
ckleinschmidt <ckleinschmidt@unicef.org>
2021-05-19 12:16:42 -0400

    Fix typo

--- Source/Icebreaker/Services/MatchingService.cs
+++ Source/Icebreaker/Services/MatchingService.cs
@@ -224,8 +224,8 @@ namespace Icebreaker.Services
                 for (LinkedListNode<ChannelAccount> restOfQueue = queue.First; restOfQueue != null; restOfQueue = restOfQueue.Next)
                 {
                     pairUserTwo = restOfQueue.Value;
-                    UserInfo pairUserOneInfo = this.GetOrCreateUserInfoAsync(GetChannelUserObjectId(pairUserOne, teamModel))?.Result;
-                    UserInfo pairUserTwoInfo = this.GetOrCreateUserInfoAsync(GetChannelUserObjectId(pairUserTwo, teamModel))?.Result;
+                    UserInfo pairUserOneInfo = this.GetOrCreateUserInfoAsync(GetChannelUserObjectId(pairUserOne), teamModel)?.Result;
+                    UserInfo pairUserTwoInfo = this.GetOrCreateUserInfoAsync(GetChannelUserObjectId(pairUserTwo), teamModel)?.Result;
 
                     this.telemetryClient.TrackTrace($"Processing {pairUserOneInfo?.UserId} and {pairUserTwoInfo?.UserId}");
 
