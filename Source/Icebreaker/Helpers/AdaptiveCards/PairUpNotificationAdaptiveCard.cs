5efd00127a4632cee82b5106d830dae3ec884fc1
vedavyasaHS <39366534+vedavyasaHS@users.noreply.github.com>
2021-06-04 19:48:22 +0530

    Update PairUpNotificationAdaptiveCard.cs

--- Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
+++ Source/Icebreaker/Helpers/AdaptiveCards/PairUpNotificationAdaptiveCard.cs
@@ -54,7 +54,7 @@ namespace Icebreaker.Helpers.AdaptiveCards
 
             var matchUpCardTitleContent = Resources.MatchUpCardTitleContent;
             var matchUpCardMatchedText = string.Format(Resources.MatchUpCardMatchedText, recipient.Name);
-            var matchUpCardContentPart1 = string.Format(Resources.MatchUpCardContentPart1, botDisplayName, teamName, recipient.Name);
+            var matchUpCardContentPart1 = string.Format("Hello", botDisplayName, teamName, recipient.Name);
             var matchUpCardContentPart2 = Resources.MatchUpCardContentPart2;
             var chatWithMatchButtonText = string.Format(Resources.ChatWithMatchButtonText, recipientGivenName);
             var pauseMatchesButtonText = Resources.PausePairingsButtonText;
